efd227babf6d009f1a5feaa8f4b509669ab9928a
DilmyPerera <samadiniperera20@gmail.com>
2026-09-04 12:29:53 +0530

    feat(jwt): add utility functions for signing and verifying access and refresh tokens

---
+++ server/src/utils/jwt.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const env = require('../config/env');
+
+function signAccessToken(user) {
+  return jwt.sign({ sub: user.id, role: user.role }, env.jwt.accessSecret, {
+    expiresIn: env.jwt.accessExpiresIn,
+  });
+}
+
+function signRefreshToken(user) {
+  return jwt.sign({ sub: user.id, role: user.role }, env.jwt.refreshSecret, {
+    expiresIn: env.jwt.refreshExpiresIn,
+  });
+}
+
+function verifyAccessToken(token) {
+  return jwt.verify(token, env.jwt.accessSecret);
+}
+
+function verifyRefreshToken(token) {
+  return jwt.verify(token, env.jwt.refreshSecret);
+}
+
+module.exports = {
+  signAccessToken,
+  signRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+};
